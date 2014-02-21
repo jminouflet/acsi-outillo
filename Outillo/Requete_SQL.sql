658b9878f6bc21cd5dca1c67644d8e5a6832fc0c
jminouflet <jean.minouflet@orion-solution.com>
2014-02-21 16:14:16 +0100

    correction sql

--- Outillo/Requete_SQL.sql
+++ Outillo/Requete_SQL.sql
@@ -1,63 +1,72 @@
 //Table CategorieClient
 
 Create table catégorieclient{
-CodeCategorieClient number(2) not null,
-NomCategorieClient varchar(50) not null,
-constraint PK_client primary key(CodeClient)
+	CodeCategorieClient 	number(2) 		not null,
+	NomCategorieClient 		varchar(50) 	not null,
+constraint PK_client 
+				primary key(CodeClient)
 };
 
 //Table Client
 
 Create table client{
-CodeClient    number(5)   not null,
-NomClient   varchar(50)  not null,
-ContactClient  varchar(50)  not null,
-NumeroRueClient  varchar(6)   not null,
-NomRueClient  varchar(100) not null,
-CodePostalClient varchar(6)   not null,
-VilleClient   varchar(100)  not null,
-EmailContactClient varchar(100)  not null,
-NumeroTelClient  char(10),
-constraint PK_client primary key(CodeClient)
+	CodeClient   		number(5)   	not null,
+	NomClient   		varchar(50)  	not null,
+	ContactClient  		varchar(50)  	not null,
+	NumeroRueClient  	varchar(6)   	not null,
+	NomRueClient  		varchar(100) 	not null,
+	CodePostalClient 	varchar(6)   	not null,
+	VilleClient   		varchar(100)  	not null,
+	EmailContactClient 	varchar(100)  	not null,
+	NumeroTelClient  	char(10),
+constraint PK_client 
+				primary key(CodeClient)
 };
 
 //Table Agence
  
 Create table agence{
-CodeAgence    number(2)   not null,
-NomAgence   varchar(50)  not null,
-NumeroRueAgence  varchar(50)  not null,
-NomRueAgence  varchar(6)   not null,
-CodePostalAgence char(5)   not null,
-VilleAgence   varchar(6)   not null,
-constraint PK_agence primary key(CodeAgence)
+	CodeAgence    	 	number(2)    not null,
+	NomAgence  		 	varchar(50)  not null,
+	NumeroRueAgence  	varchar(50)  not null,
+	NomRueAgence  		varchar(6)   not null,
+	CodePostalAgence 	char(5)   	 not null,
+	VilleAgence   		varchar(6)   not null,
+constraint PK_agence 
+				primary key(CodeAgence)
 };
 
 //Table Catégorie
 
 Create table categorie{
-CodeCategorie number(2) not null,
-NomCategorie varchar(50) not null
+	CodeCategorie 		number(2) 		not null,
+	NomCategorie 		varchar(50) 	not null
 };
 
 //Table produit
 
 Create table produit{
-CodeProduit number(5) not null,
-CodeTVA number(2) not null,
-DenominationProduit varchar(50),
-constraint PK_produit primary key(CodeProduit),
-Constraint FK_produit_TVA foreign key (CodeTVA) references TVA(CodeTVA)
+	CodeProduit 		number(5) 		not null,
+	CodeTVA 			number(2) 		not null,
+	DenominationProduit varchar(50),
+constraint PK_produit 
+				primary key(CodeProduit),
+Constraint FK_produit_TVA 
+				foreign key (CodeTVA) 
+				references TVA(CodeTVA)
 };
 
 //Table Commande
 
 Create table commande{
-CodeCommande number (5) not null,
-CodeClient number (5) not null,
-DateCommande date,
-constraint PK_commande primary key(CodeCommande),
-constraint FK_commande_client foreign key (CodeClient) references client(CodeClient)
+	CodeCommande 		number(5) 		not null,
+	CodeClient 			number(5) 		not null,
+	DateCommande 		date,
+constraint PK_commande 
+				primary key(CodeCommande),
+constraint FK_commande_client 
+				foreign key (CodeClient) 
+				references client(CodeClient)
 };
 
 
