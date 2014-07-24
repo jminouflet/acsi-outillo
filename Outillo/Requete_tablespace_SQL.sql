1b64f9fedfb4932a9f3ae27679579d3a164f9ff0
b2orislave <boris.liberkowski@gmail.com>
2014-07-24 11:11:02 +0200

    update and add
    add tablespace.sql

---
+++ Outillo/Requete_tablespace_SQL.sql
@@ -0,0 +1,79 @@
+create tablespace TS_MOY01
+datafile 'C:\Users\b2orislave\Desktop\DISK\DISK_1\TS_MOY01.dbf'
+size 4310K
+autoextend on
+segment space management auto
+extent management local uniform size 560K
+
+create tablespace TS_MOY04
+datafile 'C:\Users\b2orislave\Desktop\DISK\DISK_1\TS_MOY04.dbf'
+size 80K
+autoextend on
+segment space management auto
+extent management local uniform size 40K
+
+create tablespace TS_SOUV05
+datafile 'C:\Users\b2orislave\Desktop\DISK\DISK_1\TS_SOUV05.dbf'
+size 500K
+autoextend on
+segment space management auto
+extent management local uniform size 40K
+
+create tablespace TS_MOY02
+datafile 'C:\Users\b2orislave\Desktop\DISK\DISK_2\TS_MOY02.dbf'
+size 1140K
+autoextend on
+segment space management auto
+extent management local uniform size 150K
+
+create tablespace TS_SOUV02
+datafile 'C:\Users\b2orislave\Desktop\DISK\DISK_2\TS_SOUV02.dbf'
+size 900K
+autoextend on
+segment space management auto
+extent management local uniform size 120K
+
+create tablespace TS_SOUV04
+datafile 'C:\Users\b2orislave\Desktop\DISK\DISK_2\TS_SOUV04.dbf'
+size 540K
+autoextend on
+segment space management auto
+extent management local uniform size 70K
+
+create tablespace TS_MOY03
+datafile 'C:\Users\b2orislave\Desktop\DISK\DISK_3\TS_MOY03.dbf'
+size 400K
+autoextend on
+segment space management auto
+extent management local uniform size 50K
+
+create tablespace TS_PEU01
+datafile 'C:\Users\b2orislave\Desktop\DISK\DISK_3\TS_PEU01.dbf'
+size 400K
+autoextend on
+segment space management auto
+extent management local uniform size 40K
+
+create tablespace TS_SOUV01
+datafile 'C:\Users\b2orislave\Desktop\DISK\DISK_3\TS_SOUV01.dbf'
+size 1025K
+autoextend on
+segment space management auto
+extent management local uniform size 130K
+
+create tablespace TS_SOUV03
+datafile 'C:\Users\b2orislave\Desktop\DISK\DISK_3\TS_SOUV3.dbf'
+size 750K
+autoextend on
+segment space management auto
+extent management local uniform size 100K
+
+create tablespace TS_SOUV06
+datafile 'C:\Users\b2orislave\Desktop\DISK\DISK_3\TS_SOUV3.dbf'
+size K
+autoextend on
+segment space management auto
+extent management local uniform size 100K
+
+
+
